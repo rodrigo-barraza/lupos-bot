b39a44b755bb487e7868e3e48ef21b4ef8cde59a
Rodrigo Barraza <hello@rod.dev>
2026-07-05 08:56:06 -0700

    chore: remove obsolete fix scripts used for refactoring deathroll utilities

--- fix3.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/commands/utility/deathrollUtils.ts', 'utf8');
-
-code = code.replace(/\(game\.initiator as string\)Name/g, 'game.initiatorName');
-code = code.replace(/\(game\.opponent as string\)Name/g, 'game.opponentName');
-code = code.replace(/\(game\.opponent as string\) = userId;/g, 'game.opponent = userId;');
-
-fs.writeFileSync('src/commands/utility/deathrollUtils.ts', code);
